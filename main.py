586d0a2de4dc669ae3fc6ab0f98959924a4bccd4
Loren McGinnis <mcginnis.loren@gmail.com>
2014-11-03 22:35:28 -0700

    got bottle working

--- main.py
+++ main.py
@@ -4,10 +4,10 @@ from bottle import route, run, view, static_file, redirect, request, response, d
 
 
 @route('/candyland')
-def static(path):
+def static():
     return static_file('sim.html', root='.')
 
 # run(host='0.0.0.0', port=8080)
-# app = run(host='localhost', port=8080, reloader=True, debug=True)
-app = default_app()
+app = run(host='localhost', port=8080)
+#app = default_app()
 
