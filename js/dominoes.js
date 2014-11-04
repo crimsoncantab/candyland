b27f475c624c67cc42205746a1c8ef0a8b01f9d7
Loren McGinnis <mcginnis.loren@gmail.com>
2014-11-04 16:20:51 -0700

    static stuff through bottle, random dominoes

--- js/dominoes.js
+++ js/dominoes.js
@@ -1,7 +1,12 @@
+function rand_int(min,max)
+{
+    return ~~(Math.random()*(max-min+1)+min);
+}
+
 angular.module('dominoes', [])
   .controller('DominoesController', function() {
     this.available = []
     this.add = function() {
-        this.available.push([1,2])
+        this.available.push([rand_int(1,6),rand_int(1,6)])
     }
   });
--- main.py
+++ main.py
@@ -4,8 +4,16 @@ from bottle import route, run, view, static_file, redirect, request, response, d
 
 
 @route('/candyland')
-def static():
+def candyland():
     return static_file('sim.html', root='.')
+
+@route('/dominoes')
+def dominoes():
+    return static_file('dominoes.html', root='.')
+
+@route('/<folder:re:(css|fonts|js|images)>/<path:path>')
+def static(folder, path):
+    return static_file(path, root=folder)
 
 # run(host='0.0.0.0', port=8080)
 app = run(host='localhost', port=8080)
